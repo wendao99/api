547e1a42bc2ddfd05710c7023d23942c9a38903c
unknown <15767518757@163.com>
2021-07-26 10:17:41 +0800

    san

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ module.exports = (req, res) => {
 
   if (req.body && req.body.who) {
     who = req.body.who;
+    console.log(req.body.who);
   } else if (req.query.who) {
     who = req.query.who;
+    console.log(req.query.who);
   } else if (req.cookies.who) {
     who = req.cookies.who;
+    console.log(req.cookies.who);
   }
 
   res.status(200).send(`Hello ${who}!`);
